b6a3280388d57dc378a7ffe0e9b1dfbfdd130a72
Estêvão Carlos-Lima <104726429+estevaoclima@users.noreply.github.com>
2026-02-05 20:43:12 -0300

    Refactor conectar_planilha for Streamlit compatibility
    Updated the conectar_planilha function to work with Streamlit's cloud system. Adjusted the method of retrieving credentials and spreadsheet ID.

--- google_sheets.py
+++ google_sheets.py
@@ -8,20 +8,34 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
-SPREADSHEET_ID = "1o06Ef6zUhm2LaLd5FII-CgGpoBkPkUL8cb0XDlhA1eg" # pedaco do site do nome
+import streamlit as st
+import gspread
+
+##SPREADSHEET_ID = "1o06Ef6zUhm2LaLd5FII-CgGpoBkPkUL8cb0XDlhA1eg" # pedaco do site do nome
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-def conectar_planilha():
-    creds = Credentials.from_service_account_file(
-        "credentials.json",
+
+
+#def conectar_planilha():
+#    creds = Credentials.from_service_account_file(
+#        "credentials.json",
+#        scopes=SCOPES
+#    )
+#    client = gspread.authorize(creds)
+#    sheet = client.open_by_key(SPREADSHEET_ID).sheet1
+#    return sheet
+def conectar_planilha(): # new version to work online using streamlite cloude system. The previous would work fine, but locally
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
         scopes=SCOPES
     )
+
     client = gspread.authorize(creds)
-    sheet = client.open_by_key(SPREADSHEET_ID).sheet1
+    sheet = client.open_by_key(st.secrets["spreadsheet_id"]).sheet1
     return sheet
 
 def adicionar_movimentacao(mov):
@@ -39,4 +53,5 @@ def carregar_dados():
     sheet = conectar_planilha()
     dados = sheet.get_all_records()
     return pd.DataFrame(dados)
+
 
